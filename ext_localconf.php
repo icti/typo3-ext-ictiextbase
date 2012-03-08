acc39179f56da56224c2e7cf16156420f6f2b226
Jose Antonio Guerra <jaguerra@icti.es>
2012-03-08 18:10:40 +0100

    Generamos cHash para todas las peticiones
    Cuando una petición a TYPO3 tiene argumentos adicionales (diferentes de id, type, etc...) y la página es cacheable, este caché se identifica por un parámetro que debe ser facilitado (cHash).
    
    Si la petición lleva el cHash correcto la página se dibuja y se almacena en la caché de páginas de TYPO3 para las siguientes peticiones. Hasta ahí correcto.
    
    El problema viene si una petición no lleva cHash. En ese caso los argumentos adicionales no se tienen en cuenta para el cHash, esto es, para la identificación en caché. Esa página se pinta y se almacena.
    
    Si luego llega una petición a la misma página pero con otros parámetros y sigue sin llevar cHash, entonces se sirve del caché la primera página y no la página que se debería generar con los parámetros nuevos.
    
    Para corregirlo añadimos una función que en caso de recibir una petición con argumentos adicionales y no lleva cHash, genera uno en base a ellos, almacenando de esta forma la versión correcta en la caché.
    
    Relates #10811, #10291

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,4 +9,6 @@ require_once(t3lib_extMgm::extPath($_EXTKEY).'/Classes/Helpers/Exception.php');
 require_once(t3lib_extMgm::extPath($_EXTKEY).'/Classes/Helpers/Exception/TableNotInTCA.php');
 
 
+//$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/index_ts.php']['preprocessRequest'][] = 'EXT:ictiextbase/Classes/Helpers/PreprocessRequestHook.php:Tx_Ictiextbase_Helpers_PreprocessRequestHook->preprocessRequest';
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['checkAlternativeIdMethods-PostProc'][] = 'EXT:ictiextbase/Classes/Helpers/PreprocessRequestHook.php:Tx_Ictiextbase_Helpers_PreprocessRequestHook->checkAlternativeIdMethodsPostProc';
 ?>

b65db341839326f0488431965071675e26eb5c46
Jose Antonio Guerra <jaguerra@icti.es>
2012-03-23 12:02:53 +0100

    Extendemos DateTime para gestionar cálculos de recurrencias
    Relates #10804

---
+++ Classes/Helpers/DateTime.php
@@ -0,0 +1,107 @@
+<?php
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * 
+ *
+ * @package Ictiextbase
+ * @subpackage Helpers
+ * @scope prototype
+ * @api
+ */
+class Tx_Ictiextbase_Helpers_DateTime extends DateTime  {
+
+	/**
+	 *
+	 * @return int Last day of the current date's month 
+	 */
+	public function getLastDayOfMonth() {
+		
+		$month = $this->format('n');
+		$year = $this->format('Y');
+		
+		$result = strtotime("{$year}-{$month}-01"); 
+		$result = strtotime('-1 second', strtotime('+1 month', $result)); 
+		return date('d', $result); 
+	} 
+
+	public function addMonths($delta) { 
+		
+		$month = $this->format('n');
+		$year = $this->format('Y');
+		$day = $this->format('j');
+		$hour = $this->format('G');
+		$minute = $this->format('i');
+		$second = $this->format('s');
+		
+		$month += $delta;
+		
+		$monthDate = new Tx_Ictiextbase_Helpers_DateTime;
+		$monthDate->setDate($year, $month, 1);
+		$monthDate->setTime(0,0);
+		
+		if($monthDate->getLastDayOfMonth() < $day){
+			$day = $monthDate->getLastDayOfMonth();
+		}
+		
+		$this->setDate($year, $month, $day);
+		$this->setTime($hour, $minute, $second);
+		
+	} 	
+	
+	public function diffMonths(Tx_Ictiextbase_Helpers_DateTime $diffDate){
+		$thisDateMonth = clone $this;
+		$thisDateMonth->toFirstDayOfMonth();
+		
+		$diffDateMonth = clone $diffDate;
+		$diffDateMonth->toFirstDayOfMonth();
+		
+		/**
+		 * Can't use DateDiff, need to be PHP 5.2 compatible...
+		 */
+		$diffTstamp =  $diffDateMonth->format('U') - $thisDateMonth->format('U');
+
+		$diffGetDate = getdate($diffTstamp);
+		$diffMonths =  ($diffGetDate['year'] - 1970) * 12 + ($diffGetDate['mon'] - 1);		
+		return $diffMonths;
+		
+	}
+	
+	/**
+	 *
+	 * @return Tx_Ictiextbase_Helpers_DateTime 
+	 */
+	public function toFirstDayOfMonth(){
+		$month = $this->format('n');
+		$year = $this->format('Y');	
+		$this->setDate($year, $month, 1);
+		$this->setTime(0,0);
+		return $this;
+	}
+	
+	public function __toString (){
+		return $this->format('c');
+	}
+}
+?>
---
+++ Tests/Unit/Helpers/DateTimeTest.php
@@ -0,0 +1,169 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DateTimeTest
+ *
+ * @author jaguerra
+ */
+class Tx_Ictiextbase_Helpers_DateTimeTest  extends Tx_Extbase_Tests_Unit_BaseTestCase  {
+
+	
+	/**
+	 * @var Tx_Ictiextbase_Helpers_DateTime
+	 */
+	protected $fixture;
+
+	public function setUp() {
+		$this->fixture = new Tx_Ictiextbase_Helpers_DateTime();
+	}
+
+	public function tearDown() {
+		unset($this->fixture);
+	}	
+	
+	/**
+	 * @test
+	 */
+	public function lastDayOfMonth() {
+		
+        $this->fixture->setDate(1990, 1, 31);
+        $this->fixture->setTime(0,0);		
+		
+		$this->assertEquals(
+			31,
+			$this->fixture->getLastDayOfMonth()
+		);	
+		
+        $this->fixture->setDate(1990, 2, 1);
+        $this->fixture->setTime(0,0);		
+		
+		$this->assertEquals(
+			28,
+			$this->fixture->getLastDayOfMonth()
+		);	
+		
+        $this->fixture->setDate(1992, 2, 1);
+        $this->fixture->setTime(0,0);		
+		
+		$this->assertEquals(
+			29,
+			$this->fixture->getLastDayOfMonth()
+		);		
+	}
+	
+	/**
+	 * @test
+	 */
+	public function addMonths() {
+        $this->fixture->setDate(1990, 1, 31);
+        $this->fixture->setTime(0,0);	
+		
+		$this->assertEquals(
+			'1990-1-31',
+			$this->fixture->format('Y-n-j')
+		);		
+		
+		$date = clone $this->fixture;
+		$date->addMonths(1);
+		$this->assertEquals(
+			'1990-2-28',
+			$date->format('Y-n-j')
+		);	
+		
+		$date = clone $this->fixture;
+		$date->addMonths(25);
+		$this->assertEquals(
+			'1992-2-29',
+			$date->format('Y-n-j')
+		);			
+	}	
+	
+	
+	/**
+	 * @test
+	 */
+	public function addMonths2() {
+        $this->fixture->setDate(1990, 1, 10);
+        $this->fixture->setTime(0,0);	
+		
+		$this->assertEquals(
+			'1990-1-10',
+			$this->fixture->format('Y-n-j')
+		);		
+		
+		$date = clone $this->fixture;
+		$date->addMonths(1);
+		$this->assertEquals(
+			'1990-2-10',
+			$date->format('Y-n-j')
+		);	
+		
+		$date = clone $this->fixture;
+		$date->addMonths(25);
+		$this->assertEquals(
+			'1992-2-10',
+			$date->format('Y-n-j')
+		);			
+	}	
+	
+	/**
+	 * @test
+	 */
+	public function diffMonths() {
+				
+        $this->fixture->setDate(1990, 1, 31);
+        $this->fixture->setTime(0,0);	
+		
+		
+		$date = new Tx_Ictiextbase_Helpers_DateTime;
+		$date->setDate(1990, 2, 28);
+		$date->setTime(0,0);
+		
+		$this->assertEquals(
+			1,
+			$this->fixture->diffMonths($date)
+		);	
+		
+		$date = new Tx_Ictiextbase_Helpers_DateTime;
+		$date->setDate(1991, 3, 15);
+		$date->setTime(0,0);
+		
+		$this->assertEquals(
+			14,
+			$this->fixture->diffMonths($date)
+		);	
+		
+		$date = new Tx_Ictiextbase_Helpers_DateTime;
+		$date->setDate(1989, 12, 28);
+		$date->setTime(0,0);
+		
+		$this->assertEquals(
+			-1,
+			$this->fixture->diffMonths($date)
+		);			
+		
+	
+	}	
+	
+	/**
+	 * @test
+	 */	
+	public function toFirstDayOfMonth(){
+        $this->fixture->setDate(1990, 1, 31);
+        $this->fixture->setTime(0,0);	
+		
+		$this->assertEquals(
+			'1990-1-1',
+			$this->fixture->toFirstDayOfMonth()->format('Y-n-j')
+		);		
+	}
+	
+	
+}
+
+?>

1f05063477af0e5d87b3a1dd640daad885ea123b
Jose Antonio Guerra <jaguerra@icti.es>
2013-06-25 13:48:59 +0200

    [BUGFIX] Absolute links on media are not working

--- Classes/ViewHelpers/Link/MediaViewHelper.php
+++ Classes/ViewHelpers/Link/MediaViewHelper.php
@@ -64,6 +64,7 @@ class Tx_Ictiextbase_ViewHelpers_Link_MediaViewHelper extends Tx_Fluid_Core_View
 
 	/**
 	 * @param Tx_Icticontent_Domain_Model_Media $media DAM file to create the link
+	 * @param boolean $absolute If set, an absolute URI is rendered
 	 * @return string Rendered page URI
 	 */
 	public function render(Tx_Ictiextbase_Domain_Model_MediaInterface $media) {
@@ -71,12 +72,38 @@ class Tx_Ictiextbase_ViewHelpers_Link_MediaViewHelper extends Tx_Fluid_Core_View
 		$uri = $uriBuilder
 			->reset()
 			->setTargetPageUid( $this->sanitizeFileUri( $media->getMediaUri() ) )
+			->setCreateAbsoluteUri($absolute)
 			->build();
+
+		if($absolute !== FALSE){
+				$uri = $this->forceAbsoluteUrl($uri);
+		}
 
 		$this->tag->addAttribute('href', $uri);
 		$this->tag->setContent($this->renderChildren());
 
 		return $this->tag->render();
+	}
+
+	protected function forceAbsoluteUrl($url){
+
+			$urlParts = parse_url($url);
+
+			// Set scheme and host if not yet part of the URL:
+			if (empty($urlParts['host'])) {
+					$url = implode('', array( $this->getSiteUrl(), $url ));
+			}
+
+			return $url;
+
+	}
+
+	protected function getSiteUrl(){
+			if(!empty($GLOBALS['TSFE']->absRefPrefix)){
+					return $GLOBALS['TSFE']->absRefPrefix;
+			} else {
+					return t3lib_div::getIndpEnv('TYPO3_SITE_URL');
+			}
 	}
 	
 	protected function sanitizeFileUri($fileUri){
--- Classes/ViewHelpers/Uri/MediaViewHelper.php
+++ Classes/ViewHelpers/Uri/MediaViewHelper.php
@@ -16,9 +16,36 @@ class Tx_Ictiextbase_ViewHelpers_Uri_MediaViewHelper extends Tx_Fluid_Core_ViewH
 			->setCreateAbsoluteUri($absolute)
 			->build();
 
+		if($absolute !== FALSE){
+				$uri = $this->forceAbsoluteUrl($uri);
+		}
+
 		return $uri;
 	}
-	
+
+
+	protected function forceAbsoluteUrl($url){
+
+			$urlParts = parse_url($url);
+
+			// Set scheme and host if not yet part of the URL:
+			if (empty($urlParts['host'])) {
+					$url = implode('', array( $this->getSiteUrl(), $url ));
+			}
+
+			return $url;
+
+	}
+
+
+	protected function getSiteUrl(){
+			if(!empty($GLOBALS['TSFE']->absRefPrefix)){
+					return $GLOBALS['TSFE']->absRefPrefix;
+			} else {
+					return t3lib_div::getIndpEnv('TYPO3_SITE_URL');
+			}
+	}
+
 	protected function sanitizeFileUri($fileUri){
 		return str_replace(' ','%20',$fileUri);
 	}
